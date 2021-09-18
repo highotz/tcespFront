8ae1ba9a534d1456e747255d4d086b6c530ce920
Gabriel Lima <gabriel.lima@MacBook-de-Gabriel.local>
2021-09-18 15:42:52 -0300

    pagina cadastro de municipio criada

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route } from 'react-router-dom'
 
 import { Login } from './pages/Login'
+import {CadastroMunicipio} from './pages/CadastroMunicipio';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path='/' exact component={Login} />
+      {/* <Route path='/' exact component={Login} /> */}
+      <Route path='/' exact component={CadastroMunicipio} />
   </BrowserRouter>
   );
 }
